acc901f7ecafe425865eebdac54d7def7c5db415
young <zerobugpark@gmail.com>
2025-02-18 19:53:32 +0900

    홈 화면 테이블 뷰 구현

--- RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/ViewController.swift
@@ -7,11 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
+import RxSwift
+import RxCocoa
 
+final class ViewController: UIViewController {
+
+    private let tableView = UITableView()
+    
+    let items = Observable.just([
+        "SimpleTable",
+        "NumbersView",
+        "SimpleValidation",
+        "BirthDayView"
+    ])
+    
+    
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configruation()
+        bind() 
+    }
+    
+    
+    
+    
+    private func configruation() {
+        view.addSubview(tableView)
+        
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
+        
+        
+        tableView.rowHeight = 100
+        
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: TableViewCell.id)
+    }
+    
+    private func bind() {
+        
+        items
+            .bind(to: tableView.rx.items(cellIdentifier: TableViewCell.id, cellType: TableViewCell.self)) { (row, element, cell) in
+                
+                cell.titleLabel.text = element
+                
+            }.disposed(by: disposeBag)
+        
     }
 
 
