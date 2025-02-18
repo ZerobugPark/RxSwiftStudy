f7762c768efccefa1b9cbeecb5a339f0bf4e473c
young <zerobugpark@gmail.com>
2025-02-19 00:26:05 +0900

    .

--- RxSwiftStudy/NumbersView/NumbersViewController.swift
+++ RxSwiftStudy/NumbersView/NumbersViewController.swift
@@ -7,15 +7,101 @@
 
 import UIKit
 
-class NumbersViewController: UIViewController {
+import SnapKit
+import RxSwift
+import RxCocoa
 
+final class NumbersViewController: UIViewController {
+
+    let firstTextField = UITextField()
+    let secondTextField = UITextField()
+    let thirdTextField = UITextField()
+    let imageView = UIImageView()
+
+    let resultLabel = UILabel()
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        configuration()
+        bind()
+        view.backgroundColor = .white
     }
+    
+    private func configuration() {
+        view.addSubview(firstTextField)
+        view.addSubview(secondTextField)
+        view.addSubview(thirdTextField)
+        view.addSubview(resultLabel)
+        view.addSubview(imageView)
+        
+        
+        firstTextField.snp.makeConstraints { make in
+            make.bottom.equalTo(secondTextField.snp.top).offset(-20)
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+            make.centerX.equalTo(view)
+        }
+        secondTextField.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
+        thirdTextField.snp.makeConstraints { make in
+            make.top.equalTo(secondTextField.snp.bottom).offset(20)
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+            make.centerX.equalTo(view)
+        }
+        imageView.snp.makeConstraints { make in
+            make.size.equalTo(30)
+            make.trailing.equalTo(thirdTextField.snp.leading).offset(-10)
+            make.top.equalTo(secondTextField.snp.bottom).offset(30)
+        }
+        
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(thirdTextField.snp.bottom).offset(5)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(280)
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+            
+        }
+        imageView.image = UIImage(systemName: "plus")
+     
+        firstTextField.borderStyle = .roundedRect
+        firstTextField.textAlignment = .right
+        firstTextField.layer.borderWidth = 5
+        secondTextField.borderStyle = .roundedRect
+        firstTextField.textAlignment = .right
+        secondTextField.layer.borderWidth = 5
+        thirdTextField.borderStyle = .roundedRect
+        firstTextField.textAlignment = .right
+        thirdTextField.layer.borderWidth = 5
+
+        
+    }
+    
+    
+    private func bind() {
+        
+        // combineLatest 여러개 옵저버들 등록이 가능
+        // resultSelector까지 추가 가능
+        // firstTextField.rx.text.orEmpty 문자열이 비어있지 않다면
+        Observable.combineLatest(firstTextField.rx.text.orEmpty, secondTextField.rx.text.orEmpty, thirdTextField.rx.text.orEmpty) { value, value2, value3 -> Int in
+            return (Int(value) ?? 0) + (Int(value2) ?? 0) + (Int(value3) ?? 0)
+        }.map { $0.description } // Int -> String으로 변환
+        .bind(to: resultLabel.rx.text) // 텍스트에 값 바로 대입
+        .disposed(by: disposeBag)
+        
+  
+    }
+        
+            
+    
+}
     
 
   
 
-}
+
--- RxSwiftStudy/SimpleValidation/SimpleValidationViewController.swift
+++ RxSwiftStudy/SimpleValidation/SimpleValidationViewController.swift
@@ -7,14 +7,171 @@
 
 import UIKit
 
-class SimpleValidationViewController: UIViewController {
+import SnapKit
 
+import RxSwift
+import RxCocoa
+
+final class SimpleValidationViewController: UIViewController {
+
+    private let userTitle = UILabel()
+    private let userNameTextField = UITextField()
+    private let userValidationLabel = UILabel()
+    
+    private let passwordTitle = UILabel()
+    private let passwordTextField = UITextField()
+    private let passwordValidationLabel = UILabel()
+    
+    private let button = UIButton()
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .white
+        configuration()
+        bind()
     }
+    
+    
+    private func bind() {
+        
+        //Observerable 생성
+        let usernameVaild = userNameTextField
+            .rx
+            .text
+            .orEmpty
+            .map { $0.count >= 5 }
+            .share(replay: 1) // share를 통해 구독하고 있는 친구들에게 값을 전달 (reply 버퍼의 수)
+        
+        let passwordVaild = passwordTextField
+            .rx
+            .text
+            .orEmpty
+            .map { $0.count >= 5 }
+            .share(replay: 1) // share를 통해 구독하고 있는 친구들에게 값을 전달 (reply 버퍼의 수)
+        // 여러개의 Observerable을 생성할 필요가 없음
+        
+        //유저네임과 패스워드와 조건 확인
+        let everythingValid = Observable.combineLatest(usernameVaild, passwordVaild).map { $0 && $1}
+        
+        usernameVaild
+            .bind(to: passwordTextField.rx.isEnabled) //유저네임이 조건이 맞아야 비밀번호 입력 가능
+            .disposed(by: disposeBag)
+        
+        usernameVaild
+            .bind(to: userValidationLabel.rx.isHidden) //유저네임 조건이 맞을 경우 안내 레이블 히든
+            .disposed(by: disposeBag)
+        
+        passwordVaild
+            .bind(to: passwordValidationLabel.rx.isHidden) //비밀번호 조건이 맞을 경우 안내 레이블 히든
+            .disposed(by: disposeBag)
+        
+        everythingValid.bind(to: button.rx.isEnabled).disposed(by: disposeBag)
+        
+        button.rx.tap.bind(with: self) { owner, _ in
+            let title = "RxExample"
+            let msg = "가입에 성공했어요!"
+            let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+            let ok = UIAlertAction(title: "확인", style: .default)
+            alert.addAction(ok)
+            
+            owner.present(alert, animated: true)
+        }.disposed(by: disposeBag)
+
+        
+        
+    }
+
     
 
 
+}
+
+extension SimpleValidationViewController {
+    private func configuration() {
+        view.addSubview(userTitle)
+        view.addSubview(userNameTextField)
+        view.addSubview(userValidationLabel)
+        view.addSubview(passwordTitle)
+        view.addSubview(passwordTextField)
+        view.addSubview(passwordValidationLabel)
+        view.addSubview(button)
+        
+        
+        userTitle.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(20)
+            
+        }
+        
+        userNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(userTitle.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(40)
+            
+        }
+        
+        userValidationLabel.snp.makeConstraints { make in
+            make.top.equalTo(userNameTextField.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(20)
+            
+        }
+        
+        passwordTitle.snp.makeConstraints { make in
+            make.top.equalTo(userValidationLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(20)
+            
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordTitle.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(40)
+            
+        }
+        
+        passwordValidationLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(20)
+            
+        }
+        
+        button.snp.makeConstraints { make in
+            make.top.equalTo(passwordValidationLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(40)
+            
+        }
+        
+        
+        userTitle.text  = "Username"
+        userTitle.font = .systemFont(ofSize: 17)
+        passwordTitle.text  = "Password"
+        passwordTitle.font = .systemFont(ofSize: 17)
+        
+        
+        userValidationLabel.text  = "사용자 이름은 최소 5글자 이상입니다."
+        userValidationLabel.font = .systemFont(ofSize: 17)
+        userValidationLabel.textColor = .red
+        passwordValidationLabel.text  = "패스워드는 최소 5글자 이상입니다"
+        passwordValidationLabel.font = .systemFont(ofSize: 17)
+        passwordValidationLabel.textColor = .red
+        
+    
+        passwordTextField.borderStyle = .roundedRect
+        userNameTextField.borderStyle = .roundedRect
+        
+        passwordTextField.isEnabled = false
+        button.isEnabled = false
+        button.setTitle("Do SomeThing", for: .normal)
+        button.backgroundColor = .green
+        button.setTitleColor(.black, for: .normal)
+
+    }
 
 }
